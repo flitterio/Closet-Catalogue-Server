a603ee00033bdadd685564f8375ca5b19a0cf6c4
flitterio <francesca@litterio.net>
2020-12-15 23:35:49 -0500

    back on the master

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -1,10 +1,18 @@
 const express = require('express')
 
 const authRouter = express.Router()
+const jsonBodyParser = express.json()
 
 authRouter
-  .post('/login', (req, res, next) => {
-    res.send('ok')
+  .post('/signin', jsonBodyParser, (req, res, next) => {
+      const { username, password}
+    
+    for (const [key, value] of Object.entries(signinUser))
+    if(value == null)
+    return res.status(400).json({
+        error: `Missing '${key}' in request body` 
+    })
+      res.send('ok')
   })
 
 module.exports = authRouter

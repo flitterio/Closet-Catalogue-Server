424807166aa0f194f3517a300228a97e175d2e32
flitterio <francesca@litterio.net>
2021-01-06 16:01:56 -0500

    its master not main

--- src/config.js
+++ src/config.js
@@ -2,7 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/closet-catalogue',
-    TEST_DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/closet-catalogue-test',
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000/', 
     //'https://closet-catalogue.flitterio.vercel.app/',
     JWT_SECRET: process.env.JWT_SECRET || 'pizza',

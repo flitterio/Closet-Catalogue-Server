b0dd351074b591829761d78f0bc37bbb7ccb068e
flitterio <francesca@litterio.net>
2021-01-09 22:20:24 -0500

    console.log added sooner

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -13,6 +13,7 @@ authRouter
       console.log('signin user ', signinUser);
       
     for (const [key, value] of Object.entries(signinUser))
+      console.log('for key value pairs, about to check if required feilds are met')
     if(value == null)
         return (res.status(400).json({
             error: `Missing '${key}' in request body` 

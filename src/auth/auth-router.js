54f600d215c303977c37ee22498c4d4c7ff4a3c6
flitterio <francesca@litterio.net>
2021-01-09 22:12:03 -0500

    add console.log for debugging

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -14,24 +14,33 @@ authRouter
       
     for (const [key, value] of Object.entries(signinUser))
     if(value == null)
-    return res.status(400).json({
-        error: `Missing '${key}' in request body` 
-    })
+        return (res.status(400).json({
+            error: `Missing '${key}' in request body` 
+        }),
+          console.log('value==null'))
+
     AuthService.getUserWithUserName(
       req.app.get('db'),
       signinUser.username
     )
+
     .then(dbUser => {
+      console.log('dbUser', dbUser)
       if(!dbUser)
-      return res.status(400).json({
+      return (res.status(400).json({
         error: 'Incorrect username or password',
-      })
+      }),
+        
+      console.log('no dbUser'))
+      
       return AuthService.comparePasswords(signinUser.password, dbUser.password)
          .then(compareMatch => {
+           console.log('compare match', compareMatch)
            if (!compareMatch)
-             return res.status(400).json({
+             return (res.status(400).json({
                error: 'Incorrect username or password',
-             })
+             }),
+             console.log('compare didnt match'))
         const sub = dbUser.username
         const payload = { usersid: dbUser.id, fname: dbUser.fname, lname: dbUser.lname, email: dbUser.email  }
         res.send({
